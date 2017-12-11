131dde191bb6c2adec982f867258d72c6af5ddea
Dan Sullivan <dsullivan@apterainc.com>
2017-12-11 12:28:05 -0500

    add: Selenium utility methods to basePage class

---
+++ Web.UI.Tests/BasePage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Web.UI.Tests
+{
+    public class BasePage
+    {
+
+        public const int WAIT_SECONDS = 30;
+
+        IWebDriver _Driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this._Driver = driver;
+        }
+
+        public bool WaitUntilDisplayed(By locator, int maxWaitTime)
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(_Driver, System.TimeSpan.
+FromSeconds(maxWaitTime));
+                wait.Until(ExpectedConditions.ElementIsVisible(locator));
+                return true;
+            }
+            catch (OpenQA.Selenium.WebDriverTimeoutException)
+            {
+                throw new Exception("The web driver waited " + maxWaitTime + " seconds to find " + locator + " and could not find it, on the following page: " + _Driver.Url);
+            }
+        }
+    }
+}
