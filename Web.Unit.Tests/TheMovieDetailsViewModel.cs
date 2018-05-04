294db42fd879fdf93287bad676e90d6a0095d2c5
Jon Fazzaro <jfazzaro@apterainc.com>
2018-05-04 08:42:42 -0400

    feat: Censor trigger words when displaying a movie review.
    Here's what the rest of the exercise would look like, had we had time to complete it.

--- Web.Unit.Tests/TheMovieDetailsViewModel.cs
+++ Web.Unit.Tests/TheMovieDetailsViewModel.cs
@@ -10,24 +10,23 @@ using TotallyNotRobots.Movies.Web.Models;
 
 namespace TotallyNotRobots.Movies.Web.Unit.Tests
 {
-    [TestClass]
-    public class TheMovieDetailsViewModel
-    {
-		private static IApi api;
+	[TestClass]
+	public class TheMovieDetailsViewModel
+	{
+		static IApi _api;
 
 		[TestMethod]
-        public void HasAMovie()
-        {
-            Assert.AreEqual(5, GetModel(5).Movie.ID);
-        }
+		public void HasAMovie()
+		{
+			Assert.AreEqual(5, GetModel(5).Movie.ID);
+		}
 
-        [TestMethod]
-        public void LoadsTheReviewsFromTheAPI()
-        {
-            var model = GetModel(5);
-		
+		[TestMethod]
+		public void LoadsTheReviewsFromTheAPI()
+		{
+			var model = GetModel(5);
 			Assert.AreEqual(2, model.Reviews.Count());
-        }
+		}
 
 		[TestClass]
 		public class WhenCleaningAReview
@@ -39,55 +38,89 @@ namespace TotallyNotRobots.Movies.Web.Unit.Tests
 				public void EmptyString()
 				{
 					var model = GetModel(10);
-					var cleaned = model.Clean("");
-					Assert.AreEqual("", cleaned);
+					Assert.AreEqual("", model.Clean(""));
+				}
+			}
+
+			[TestClass]
+			public class GivenNoTriggerWords
+			{
+				[TestMethod]
+				public void ReturnsTheReviewUnharmed()
+				{
+					var model = GetModel(11);
+					var str = "This movie was meh.";
+					var cleaned = model.Clean(str);
+					Assert.AreEqual(str, cleaned);
 				}
 			}
 
 			[TestClass]
 			public class GivenSomeTriggerWords
 			{
+
 				[TestClass]
 				public class GivenAReviewWithNoTriggerWords
 				{
 					[TestMethod]
 					public void ReturnsTheReviewUnharmed()
 					{
-						var model = GetModel(10);
-						model.TriggerWords = new List<string>()
-						{
-							"HorseFeathers",
-							"JiveTurkey"
-						};
-
-						var str = "This movie was meh.  Just meh.";
-						var cleaned = model.Clean(str);
-						Assert.AreEqual(str, cleaned);
+						Assert.AreEqual("This movie was meh. Just meh.", 
+							Clean("This movie was meh. Just meh."));
 					}
+				}
+
+				[TestClass]
+				public class GivenAReviewWithOneTriggerWord
+				{
+					[TestMethod]
+					public void ReturnsTheCensoredReview()
+					{
+						Assert.AreEqual("Only a ********** would say that this movie was 'meh. Just meh.'", 
+							Clean("Only a JiveTurkey would say that this movie was 'meh. Just meh.'"));
+					}
+				}
+
+				[TestClass]
+				public class GivenAReviewWithTriggerWords
+				{
+					[TestMethod]
+					public void ReturnsTheCensoredReview()
+					{
+						Assert.AreEqual("Only a ********** would say that this movie was *************.", 
+							Clean("Only a JiveTurkey would say that this movie was HorseFeathers."));
+					}
+				}
+
+				static string Clean(string str)
+				{
+					var model = GetModel(12);
+					model.TriggerWords.Add("HorseFeathers");
+					model.TriggerWords.Add("JiveTurkey");
+					return model.Clean(str);
 				}
 			}
 		}
 
-        private static MovieDetailsViewModel GetModel(int id)
+		private static MovieDetailsViewModel GetModel(int id)
 		{
-			api = A.Fake<IApi>();
-			List<Review> reviews = new List<Review> {
-							new Review(),
-							new Review()
-						};
-			arrangeReviews(id, reviews);
-			return new MovieDetailsViewModel(api, new Movie { ID = id });
+			_api = A.Fake<IApi>();
+			ArrangeReviews(id, new List<Review> {
+				new Review(),
+				new Review()
+			});
+			return new MovieDetailsViewModel(_api, new Movie { ID = id });
 		}
 
-		private static void arrangeReviews(int id, List<Review> reviews)
+		private static void ArrangeReviews(int id, IList<Review> reviews)
 		{
-			A.CallTo(() => api.Reviews.GetWithHttpMessagesAsync(id,
-							A<Dictionary<string, List<string>>>.Ignored,
-							A<CancellationToken>.Ignored))
-								.Returns(new HttpOperationResponse<IList<Review>>
-								{
-									Body = reviews
-								});
+			A.CallTo(() => _api.Reviews.GetWithHttpMessagesAsync(id,
+				A<Dictionary<string, List<string>>>.Ignored,
+				A<CancellationToken>.Ignored))
+					.Returns(new HttpOperationResponse<IList<Review>>
+					{
+						Body = reviews
+					});
 		}
 	}
 }
--- Web/Models/MovieDetailsViewModel.cs
+++ Web/Models/MovieDetailsViewModel.cs
@@ -1,30 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TotallyNotRobots.Movies.Web.ApiClient;
 using TotallyNotRobots.Movies.Web.ApiClient.Models;
 
 namespace TotallyNotRobots.Movies.Web.Models
 {
-    public class MovieDetailsViewModel
-    {
-        readonly IApi _api;
-        readonly Movie _movie;
+	public class MovieDetailsViewModel
+	{
+		readonly IApi _api;
+		readonly Movie _movie;
 
-        public MovieDetailsViewModel(IApi api, Movie movie)
-        {
-            _api = api;
-            _movie = movie;
-        }
+		public MovieDetailsViewModel(IApi api, Movie movie)
+		{
+			_api = api;
+			_movie = movie;
+		}
 
-        public Movie Movie => _movie;
+		public Movie Movie => _movie;
 
-        public IEnumerable<Review> Reviews => _api.Reviews.Get(_movie.ID ?? 0);
+		public IEnumerable<Review> Reviews => _api.Reviews.Get(_movie.ID ?? 0);
 
-		public List<string> TriggerWords { get; set; }
+		public ICollection<string> TriggerWords { get; set; } = new List<string>();
 
 		public string Clean(string comments)
 		{
+			foreach (var word in TriggerWords)
+				comments = Censor(comments, word);
+
 			return comments;
+		}
+
+		static string Censor(string content, string word)
+		{
+			return content.Replace(word, new string('*', word.Length));
 		}
 	}
 }
