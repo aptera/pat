95829fbe308660fc839bf25eb67810267027780f
Jon Fazzaro <jfazzaro@apterainc.com>
2018-06-14 08:13:14 -0400

    feat: censor trigger words in review comments.

--- Web.Unit.Tests/TheMovieDetailsViewModel.cs
+++ Web.Unit.Tests/TheMovieDetailsViewModel.cs
@@ -13,7 +13,7 @@ namespace TotallyNotRobots.Movies.Web.Unit.Tests
     [TestClass]
     public class TheMovieDetailsViewModel
     {
-
+		static IApi api;
         [TestMethod]
         public void HasAMovie()
         {
@@ -27,20 +27,68 @@ namespace TotallyNotRobots.Movies.Web.Unit.Tests
             Assert.AreEqual(2, model.Reviews.Count());
         }
 
+		[TestMethod]
+		public void HasAListOfDirtyWords()
+		{
+			var model = GetModel(4);
+			CollectionAssert.AreEqual(new List<string> { "horsefeathers", "gherkin" }, model.Blacklist);
+
+		}
+
+		[TestClass]
+		public class WhenCleaningAReview
+		{
+			[TestMethod]
+			public void GivenAReviewWithAnEmptyString_ReturnsAnEmptyString()
+			{
+				AssertCleanReview("", "");
+			}
+
+			[TestMethod]
+			public void GivenAReviewWithNoDirtyWords_ReturnsReview()
+			{
+				AssertCleanReview("good movie", "good movie");
+			}
+
+			[TestMethod]
+			public void GivenAReviewWithOneDirtyWord_ReturnsACensoredReview()
+			{
+				AssertCleanReview("******* movie", "gherkin movie");
+			}
+
+			[TestMethod]
+			public void GivenAReviewWithTwoDirtyWords_ReturnsACensoredReview()
+			{
+				AssertCleanReview("******* ************* hello", "gherkin horsefeathers hello");
+			}
+
+			private static void AssertCleanReview(string Expected, string ReviewComments)
+			{
+				var model = GetModel(3);
+				Assert.AreEqual(Expected, model.Clean(ReviewComments));
+			}
+		}
+
         private static MovieDetailsViewModel GetModel(int id)
-        {
-            var api = A.Fake<IApi>();
-            A.CallTo(() => api.Reviews.GetWithHttpMessagesAsync(id,
-                A<Dictionary<string, List<string>>>.Ignored,
-                A<CancellationToken>.Ignored))
-                    .Returns(new HttpOperationResponse<IList<Review>>
-                    {
-                        Body = new List<Review> {
-                            new Review(),
-                            new Review()
-                        }
-                    });
-            return new MovieDetailsViewModel(api, new Movie { ID = id });
-        }
-    }
+		{
+			api = A.Fake<IApi>();
+			ArrangeAListOfReviews(id, new List<Review> {
+							new Review(),
+							new Review()
+						});
+
+			return new MovieDetailsViewModel(api, new Movie { ID = id });
+		}
+
+		private static void ArrangeAListOfReviews(int movieId, List<Review> list)
+		{
+			A.CallTo(() => api.Reviews.GetWithHttpMessagesAsync(movieId,
+							A<Dictionary<string, List<string>>>.Ignored,
+							A<CancellationToken>.Ignored))
+								.Returns(new HttpOperationResponse<IList<Review>>
+								{
+									Body = list
+								});
+		}
+	}
 }
--- Web/Models/MovieDetailsViewModel.cs
+++ Web/Models/MovieDetailsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using TotallyNotRobots.Movies.Web.ApiClient;
 using TotallyNotRobots.Movies.Web.ApiClient.Models;
 
@@ -13,10 +15,22 @@ namespace TotallyNotRobots.Movies.Web.Models
         {
             _api = api;
             _movie = movie;
-        }
+			this.Blacklist = new List<string> { "horsefeathers", "gherkin" }; // TODO: Make a real list
+		}
 
         public Movie Movie => _movie;
 
         public IEnumerable<Review> Reviews => _api.Reviews.Get(_movie.ID ?? 0);
-    }
+
+		public List<string> Blacklist { get; set; }
+
+		public string Clean(string reviewComments)
+		{
+			foreach (string s in Blacklist)
+			{
+				reviewComments = reviewComments.Replace(s, new String('*', s.Length));
+			}
+			return reviewComments;
+		}
+	}
 }
